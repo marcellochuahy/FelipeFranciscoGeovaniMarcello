f8b02f666eedc42595cf8118ea7d493375bdf070
marcellochuahy <marcello@chuahy.org>
2020-12-17 20:39:30 -0300

    Test ComprasTableViewController

---
+++ FelipeFranciscoGeovaniMarcello/ComprasUSATests/ComprasTableViewControllerTests.swift
@@ -0,0 +1,156 @@
+//
+//  ComprasTableViewControllerTests.swift
+//  ComprasUSATests
+//
+//  Created by Marcello Chuahy on 17/12/20.
+//  Copyright © 2020 Applause Codes. All rights reserved.
+//
+
+import XCTest
+@testable import ComprasUSA
+
+class ComprasTableViewControllerTests: XCTestCase {
+	
+	// MARK: - System Under Test
+	var sut: ComprasTableViewController!
+
+	// MARK: - Life cycle
+
+    override func setUpWithError() throws {
+		super.setUp()
+		sut = ComprasTableViewController()
+    }
+
+    override func tearDownWithError() throws {
+		sut = nil
+		super.tearDown()
+    }
+	
+	func testTitle() {
+		sut.viewDidLoad()
+		XCTAssertEqual(sut.title, "Lista de Compras")
+	}
+	
+	func testNumberOfSections_whenPurchasedProductsIsEmpty() {
+		let numberOfSections = sut.numberOfSections(in: sut.tableView!)
+		XCTAssertEqual(numberOfSections, 0)
+	}
+	
+	func testBackgroundView_whenPurchasedProductsIsEmpty() {
+		let _ = sut.numberOfSections(in: sut.tableView!)
+		XCTAssertEqual(sut.tableView.backgroundView, sut.label)
+	}
+
+	func testNumberOfSectionsIsOne_whenPurchasedProductsIsNotEmpty() {
+		appendNewPurchasedProduct()
+		let numberOfSections = sut.numberOfSections(in: sut.tableView!)
+		XCTAssertEqual(numberOfSections, 1)
+	}
+	
+	func testBackgroundViewIsNil_whenPurchasedProductsIsNotEmpty() {
+		appendNewPurchasedProduct()
+		let _ = sut.numberOfSections(in: sut.tableView!)
+		XCTAssertNil(sut.tableView.backgroundView)
+	}
+	
+	func testNumberOfRowsInSectionZeroIsOne_whenPurchasedProductsIsOne() {
+		appendNewPurchasedProduct()
+		let numberOfRowsInSectionZero = sut.tableView(sut.tableView!, numberOfRowsInSection: 0)
+		XCTAssertEqual(numberOfRowsInSectionZero, 1)
+	}
+	
+	func testNumberOfRowsInSectionZeroIsTwo_whenPurchasedProductsIsTwo() {
+		appendNewPurchasedProduct()
+		appendNewPurchasedProduct()
+		let numberOfRowsInSectionZero = sut.tableView(sut.tableView!, numberOfRowsInSection: 0)
+		XCTAssertEqual(numberOfRowsInSectionZero, 2)
+	}
+
+}
+// MARK: - Handler methods
+
+extension ComprasTableViewControllerTests {
+	
+	func appendNewPurchasedProduct() {
+		let purchasedProduct = PurchasedProduct(productName: "iPhone 12")
+		sut.purchasedProducts.append(purchasedProduct)
+	}
+	
+}
+
+
+
+
+/*
+
+
+// MARK: - Tests
+// Test name: test SUT _ whenSomethingThatNeedToHappenToStartTheTest _ SUTStateAfterWhenPhaseHappens
+func <#testName#>() {
+	
+	/// given ( set up any aditional values if needed )
+	let <#parameterName#> = <#parameterValue#>
+	
+	/// when ( execute the code being tested )
+	let actualResult = sut.<#methodToBeTested#>(<#parameterName#>)
+	
+	/// then ( assert the expected result with a message that prints if the test fails )
+	let expectedResult = "<#expectedResult: String#>"
+	XCTAssertEqual(actualResult,
+				   expectedResult,
+				   "Result should be \"\(expectedResult)\" but actually is \"\(actualResult)\"")
+}
+
+}
+
+/*
+
+Organization
+============
+
+Test Target
+⌊ Cases
+⌊ Group 1
+⌊ Test Class 1.1
+⌊ Test Class 1.2
+⌊ Group 2
+⌊ Test Class 2.1
+⌊ Test Class 2.2
+⌊ Mocks
+⌊ Helper Classes
+⌊ Helper Extensions
+
+Assert functions in XCTest:
+===========================
+
+Equality
+--------
+XCTAssertEqual
+XCTAssertNotEqual
+
+Truthiness
+----------
+XCTAssertTrue
+XCTAssertFalse
+
+Nullability
+-----------
+XCTAssertNil
+XCTAssertNotNil
+
+Comparison
+----------
+XCTAssertLessThan
+XCTAssertGreaterThan
+XCTAssertLessThanOrEqual
+XCTAssertGreaterThanOrEqual
+
+Erroring
+--------
+XCTAssertThrowsError
+XCTAssertNoThrow
+
+*/
+
+
+*/
---
+++ FelipeFranciscoGeovaniMarcello/FelipeFranciscoGeovaniMarcello/PurchasedProduct.swift
@@ -0,0 +1,19 @@
+//
+//  PurchasedProduct.swift
+//  FelipeFranciscoGeovaniMarcello
+//
+//  Created by Marcello Chuahy on 17/12/20.
+//  Copyright © 2020 Applause Codes. All rights reserved.
+//
+
+import Foundation
+
+struct PurchasedProduct {
+	
+	var productName: String
+	
+	init(productName: String) {
+		self.productName = productName
+	}
+	
+}
