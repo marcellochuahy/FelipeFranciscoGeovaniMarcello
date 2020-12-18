ad15fb747440d3579e4e50cf4967224be0c82d22
marcellochuahy <marcello@chuahy.org>
2020-12-18 13:10:38 -0300

    Set entities

---
+++ FelipeFranciscoGeovaniMarcello/FelipeFranciscoGeovaniMarcello/Models/StateOfUSA.swift
@@ -0,0 +1,21 @@
+//
+//  StateOfUSA.swift
+//  FelipeFranciscoGeovaniMarcello
+//
+//  Created by Marcello Chuahy on 18/12/20.
+//  Copyright © 2020 Applause Codes. All rights reserved.
+//
+
+import Foundation
+
+struct StateOfUSA {
+	
+	var stateName: String
+	var tax: Double
+	
+	init(stateName: String, tax: Double) {
+		self.stateName = stateName
+		self.tax = tax
+	}
+	
+}
